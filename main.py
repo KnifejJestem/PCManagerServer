ce57b37c47cb7ee43cc7fff92c0c601f3e3359d6
Mateusz Różyński <knifejjestem@gmail.com>
2026-02-14 13:02:28 +0100

    Auto stash before merge of "main" and "origin/main"

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
 # Import libraries
 
+import asyncio
 import json
-import clr
 import os
 import platform
-import asyncio
+
+import psutil
 import websockets
 import psutil
 import urllib.request
 import threading
 import time
 
-libs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "libs")
-dllpath = os.path.join(libs_dir, "LibreHardwareMonitorLib")
+os.environ["AMDSMI_GPU_METRICS_CACHE_MS"] = "200"
+from amdsmi import *  # type: ignore
 
-clr.AddReference(dllpath)
+# Get OS info
 
-from LibreHardwareMonitor.Hardware import Computer  # type: ignore
+osname = platform.system().strip()
+osver = platform.release().strip()
 
-c = Computer()
+IS_WINDOWS = "windows" in osname.lower()
 
-# Enable hardware monitoring
+if IS_WINDOWS:
+    import clr
 
-c.IsCpuEnabled = True
-c.IsGpuEnabled = True
-c.IsMemoryEnabled = True
-c.IsStorageEnabled = False
-c.IsMotherboardEnabled = False
+    libs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "libs")
+    dllpath = os.path.join(libs_dir, "LibreHardwareMonitorLib")
+
+    clr.AddReference(dllpath)  # type: ignore
+    from LibreHardwareMonitor.Hardware import Computer  # type: ignore
+
+    c = Computer()
+    # Enable hardware monitoring for Windows
+    c.IsCpuEnabled = True
+    c.IsGpuEnabled = True
+    c.IsMemoryEnabled = True
+    c.IsStorageEnabled = False
+    c.IsMotherboardEnabled = False
+    c.Open()
 
 benchmark_start = False
 
@@ -47,13 +59,14 @@ def check_and_download_presentmon():
 check_and_download_presentmon()
 
 def find_hardware():
-    global cpu_hw, gpu_hw, ram_hw
-    for hw in c.Hardware:
-        hw_type = str(hw.HardwareType)
-        if hw_type == "Cpu":
-            cpu_hw = hw
-        elif "Gpu" in hw_type:
-            gpu_hw = hw
+    if IS_WINDOWS:
+        global cpu_hw, gpu_hw, ram_hw
+        for hw in c.Hardware:
+            hw_type = str(hw.HardwareType)
+            if hw_type == "Cpu":
+                cpu_hw = hw
+            elif "Gpu" in hw_type:
+                gpu_hw = hw
 
 
 find_hardware()
@@ -62,165 +75,240 @@ disks = psutil.disk_partitions()
 
 
 def get_stats():
-    print("getstats running")
-    # Get OS info
-    osname = platform.system().strip()
-    osver = platform.release().strip()
-
-    if cpu_hw is None or gpu_hw is None or ram_hw is None:
-        find_hardware()
-    if cpu_hw:
-        cpu_hw.Update()
-    if gpu_hw:
-        gpu_hw.Update()
-
     global stats
+    if IS_WINDOWS:
+        if cpu_hw is None or gpu_hw is None or ram_hw is None:
+            find_hardware()
+        if cpu_hw:
+            cpu_hw.Update()
+        if gpu_hw:
+            gpu_hw.Update()
 
-    stats = {
-        "cpu": {
-            "name": "",
-            "usage": 0.0,
-            "temperature": 0.0,
-            "voltage": 0.0,
-            "power": 0.0,
-            "clock_speed": 0.0,
-        },
-        "gpu": {
-            "name": "",
-            "usage": 0.0,
-            "temperature": 0.0,
-            "memory_used": 0.0,
-            "memory_total": 0.0,
-            "power": 0.0,
-        },
-        "ram": {"total": 0, "used": 0},
-        "os": {"name": osname, "version": osver},
-        "disks": [],
-        "benchmarking": {
-            "fps": 0,
-            "min_fps": 0,
-            "max_fps": 0,
-            "frametime": 0.0,
-            "is_running": False,
-        },
-    }
+        stats = {
+            "cpu": {
+                "name": "",
+                "usage": 0.0,
+                "temperature": 0.0,
+                "voltage": 0.0,
+                "power": 0.0,
+                "clock_speed": 0.0,
+            },
+            "gpu": {
+                "name": "",
+                "usage": 0.0,
+                "temperature": 0.0,
+                "memory_used": 0.0,
+                "memory_total": 0.0,
+                "power": 0.0,
+            },
+            "ram": {"total": 0, "used": 0},
+            "os": {"name": osname, "version": osver},
+            "disks": [],
+            "benchmarking": {
+                "fps": 0,
+                "min_fps": 0,
+                "max_fps": 0,
+                "frametimes": 0.0,
+                "is_running": False,
+            },
+        }
 
-    while True:
+        for hw in c.Hardware:
+            hw.Update()
+            hw_type = str(hw.HardwareType)
+
+            # Get CPU stats
+            if hw_type == "Cpu":
+                stats["cpu"]["name"] = hw.Name
+                for sensor in hw.Sensors:
+                    s_type = str(sensor.SensorType)
+                    s_name = sensor.Name
+                    # print(f"Sensor: {sensor.Name}, Type: {sensor.SensorType}, Value: {sensor.Value}") # For debugging purposes
+
+                    # Usage
+
+                    if s_type == "Load" and "Total" in s_name:
+                        stats["cpu"]["usage"] = round(float(sensor.Value or 0), 1)
+
+                    # Temperature
+
+                    elif s_type == "Temperature" and "Core (Tctl/Tdie)" in s_name:
+                        stats["cpu"]["temperature"] = round(float(sensor.Value or 0), 1)
+
+                    # Clock Speed
+
+                    elif s_type == "Clock" and "Cores (Average)" in s_name:
+                        stats["cpu"]["clock_speed"] = round(float(sensor.Value or 0), 1)
+
+                    # Voltage
+
+                    elif s_type == "Voltage" and "Core" in s_name:
+                        stats["cpu"]["voltage"] = round(float(sensor.Value or 0), 2)
+
+                    # Power
+
+                    elif s_type == "Power" and "Package" in s_name:
+                        stats["cpu"]["power"] = round(float(sensor.Value or 0), 1)
+
+            # Get GPU usage and temps
+            elif "Gpu" in hw_type:
+                stats["gpu"]["name"] = hw.Name
+                for sensor in hw.Sensors:
+                    s_type = str(sensor.SensorType)
+                    # print(f"GPU Sensor: {sensor.Name}, Type: {sensor.SensorType}, Value: {sensor.Value}") # For debugging purposes
+
+                    # Usage
+
+                    if s_type == "Load" and "Core" in sensor.Name:
+                        stats["gpu"]["usage"] = round(sensor.Value, 1)
+
+                    # Temperature
+
+                    elif s_type == "Temperature" and "Core" in sensor.Name:
+                        stats["gpu"]["temperature"] = round(sensor.Value, 1)
+
+                    # Power
+
+                    elif s_type == "Power" and "GPU Package" in sensor.Name:
+                        stats["gpu"]["power"] = round(sensor.Value, 1)
+
+                    # Memory Used
+
+                    if s_type == "SmallData" and "GPU Memory Used" in sensor.Name:
+                        stats["gpu"]["memory_used"] = round(sensor.Value, 2)
+
+                    # Memory Total
+
+                    elif s_type == "SmallData" and "GPU Memory Total" in sensor.Name:
+                        stats["gpu"]["memory_total"] = round(sensor.Value, 2)
+
+            # Get RAM usage and total
+            elif hw_type == "Memory":
+                for sensor in hw.Sensors:
+                    s_type = str(sensor.SensorType)
+                    if s_type == "Data":
+                        if "Used" in sensor.Name:
+                            stats["ram"]["used"] = round(sensor.Value, 2)
+
+                        elif "Available" in sensor.Name:
+                            stats["ram"]["total"] = round(
+                                stats["ram"]["used"] + sensor.Value, 2
+                            )
+
+            # Get Disk space usage and total
+            stats["disks"] = []
+            for disk in disks:
+                # Check if the disk is a CD-ROM or has no file system (to avoid errors on Windows)
+
+                if os.name == "nt":
+                    if "cdrom" in disk.opts or disk.fstype == "":
+                        continue
+
+                # Get disk usage
+
+                usage = psutil.disk_usage(disk.mountpoint)
+
+                # Add disk info to stats
+
+                stats["disks"].append(
+                    {
+                        "number": len(stats["disks"]) + 1,
+                        "name": os.path.basename(disk.device),
+                        "device": disk.device,
+                        "mountpoint": disk.mountpoint,
+                        "fstype": disk.fstype,
+                        "total": round(usage.total / (1024**3), 2),
+                        "used": round(usage.used / (1024**3), 2),
+                        "free": round(usage.free / (1024**3), 2),
+                        "percent": usage.percent,
+                    }
+                )
+    if not IS_WINDOWS:
+        stats = {
+            "cpu": {
+                "name": "",
+                "usage": 0.0,
+                "temperature": 0.0,
+                "voltage": 0.0,
+                "power": 0.0,
+                "clock_speed": 0.0,
+            },
+            "gpu": {
+                "name": "",
+                "usage": 0.0,
+                "temperature": 0.0,
+                "memory_used": 0.0,
+                "memory_total": 0.0,
+                "power": 0.0,
+            },
+            "ram": {"total": 0, "used": 0},
+            "os": {"name": osname, "version": osver},
+            "disks": [],
+            "benchmarking": {
+                "fps": 0,
+                "min_fps": 0,
+                "max_fps": 0,
+                "frametimes": 0.0,
+                "is_running": False,
+            },
+        }
+
+        # Get GPU stats
         try:
-            for hw in c.Hardware:
-                hw.Update()
-                hw_type = str(hw.HardwareType)
+            ret = amdsmi_init(AmdSmiInitFlags.INIT_AMD_APUS)
+            devices = amdsmi_get_processor_handles()
+            if len(devices) == 0:
+                print("No GPUs on machine")
+            else:
+                for device in devices:
+                    # GPU Stats
 
-                # Get CPU stats
-                if hw_type == "Cpu":
-                    stats["cpu"]["name"] = hw.Name
-                    for sensor in hw.Sensors:
-                        s_type = str(sensor.SensorType)
-                        s_name = sensor.Name
-                        # print(f"Sensor: {sensor.Name}, Type: {sensor.SensorType}, Value: {sensor.Value}") # For debugging purposes
+                    asic_info = amdsmi_get_gpu_asic_info(device)
+                    stats["gpu"]["name"] = asic_info["market_name"]
 
-                        # Usage
+                    engine_usage = amdsmi_get_gpu_activity(device)
+                    stats["gpu"]["usage"] = engine_usage["gfx_activity"]
 
-                        if s_type == "Load" and "Total" in s_name:
-                            stats["cpu"]["usage"] = round(float(sensor.Value or 0), 1)
-
-                        # Temperature
-
-                        elif s_type == "Temperature" and "Core (Tctl/Tdie)" in s_name:
-                            stats["cpu"]["temperature"] = round(float(sensor.Value or 0), 1)
-
-                        # Clock Speed
-
-                        elif s_type == "Clock" and "Cores (Average)" in s_name:
-                            stats["cpu"]["clock_speed"] = round(float(sensor.Value or 0), 1)
-
-                        # Voltage
-
-                        elif s_type == "Voltage" and "Core" in s_name:
-                            stats["cpu"]["voltage"] = round(float(sensor.Value or 0), 2)
-
-                        # Power
-
-                        elif s_type == "Power" and "Package" in s_name:
-                            stats["cpu"]["power"] = round(float(sensor.Value or 0), 1)
-
-                # Get GPU usage and temps
-                elif "Gpu" in hw_type:
-                    stats["gpu"]["name"] = hw.Name
-                    for sensor in hw.Sensors:
-                        s_type = str(sensor.SensorType)
-                        # print(f"GPU Sensor: {sensor.Name}, Type: {sensor.SensorType}, Value: {sensor.Value}") # For debugging purposes
-
-                        # Usage
-
-                        if s_type == "Load" and "Core" in sensor.Name:
-                            stats["gpu"]["usage"] = round(sensor.Value, 1)
-
-                        # Temperature
-
-                        elif s_type == "Temperature" and "Core" in sensor.Name:
-                            stats["gpu"]["temperature"] = round(sensor.Value, 1)
-
-                        # Power
-
-                        elif s_type == "Power" and "GPU Package" in sensor.Name:
-                            stats["gpu"]["power"] = round(sensor.Value, 1)
-
-                        # Memory Used
-
-                        if s_type == "SmallData" and "GPU Memory Used" in sensor.Name:
-                            stats["gpu"]["memory_used"] = round(sensor.Value, 2)
-
-                        # Memory Total
-
-                        elif s_type == "SmallData" and "GPU Memory Total" in sensor.Name:
-                            stats["gpu"]["memory_total"] = round(sensor.Value, 2)
-
-                # Get RAM usage and total
-                elif hw_type == "Memory":
-                    for sensor in hw.Sensors:
-                        s_type = str(sensor.SensorType)
-                        if s_type == "Data":
-                            if "Used" in sensor.Name:
-                                stats["ram"]["used"] = round(sensor.Value, 2)
-
-                            elif "Available" in sensor.Name:
-                                stats["ram"]["total"] = round(
-                                    stats["ram"]["used"] + sensor.Value, 2
-                                )
-
-                # Get Disk space usage and total
-                stats["disks"] = []
-                for disk in disks:
-                    # Check if the disk is a CD-ROM or has no file system (to avoid errors on Windows)
-
-                    if os.name == "nt":
-                        if "cdrom" in disk.opts or disk.fstype == "":
-                            continue
-
-                    # Get disk usage
-
-                    usage = psutil.disk_usage(disk.mountpoint)
-
-                    # Add disk info to stats
-
-                    stats["disks"].append(
-                        {
-                            "number": len(stats["disks"]) + 1,
-                            "name": os.path.basename(disk.device),
-                            "device": disk.device,
-                            "mountpoint": disk.mountpoint,
-                            "fstype": disk.fstype,
-                            "total": round(usage.total / (1024**3), 2),
-                            "used": round(usage.used / (1024**3), 2),
-                            "free": round(usage.free / (1024**3), 2),
-                            "percent": usage.percent,
-                        }
+                    vram_memory_total = round(
+                        int(
+                            amdsmi_get_gpu_memory_total(
+                                device, amdsmi_interface.AmdSmiMemoryType.VRAM
+                            )
+                        )
+                        / 1024**3,
+                        2,
                     )
-                time.sleep(0.1)
-        except Exception as e:
-            print(f"get_stats encountered an error -> {e}")
 
+                    stats["gpu"]["memory_total"] = vram_memory_total
+                    vram_memory_usage = round(
+                        int(
+                            amdsmi_get_gpu_memory_usage(
+                                device, amdsmi_interface.AmdSmiMemoryType.VRAM
+                            )
+                        )
+                        / 1024**3,
+                        2,
+                    )
+
+                    stats["gpu"]["memory_used"] = vram_memory_usage
+                    temp_metric = amdsmi_get_temp_metric(
+                        device,
+                        AmdSmiTemperatureType.EDGE,
+                        AmdSmiTemperatureMetric.CURRENT,
+                    )
+                    stats["gpu"]["temperature"] = temp_metric
+
+                    power_info = amdsmi_get_power_info(device)
+                    stats["gpu"]["power"] = power_info["socket_power"]
+
+                    # CPU Stats
+
+                    cpu_name = amdsmi_get_cpu_model_name(device)
+                    stats["cpu"]["name"] = cpu_name.lstrip("b'").rstrip("'")
+        except AmdSmiException as e:
+            print(e)
+
+    return stats
 async def get_benchmark_stats(process):
     print("getbechstats running")
     global lines
@@ -301,8 +389,6 @@ async def stats_server(websocket):
                 pass
             else:
                 print(f"Error sending stats: {e}")
-            break
-        except websockets.ConnectionClosed:
             break
 
 
